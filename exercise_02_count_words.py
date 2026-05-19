1dc91f4f390d2f857d53b239d818ee5e20729e8f
estanislaodiego <ediego@mail.austral.edu.ar>
2026-05-19 15:59:12 -0300

    Update exercise_02_count_words.py

--- exercise_02_count_words.py
+++ exercise_02_count_words.py
@@ -28,4 +28,13 @@ def count_words(filename):
         # archivo contiene: "Hola mundo hola\nmundo python\n"
         count_words("texto.txt") -> {"hola": 2, "mundo": 2, "python": 1}
     """
-    pass  # Reemplazar con tu implementación
+    diccionario = {}
+
+    with open(filename, "r") as archivo:
+        lista = archivo.read().lower().split()
+        for i in lista:
+            if i in diccionario:
+                diccionario[i] += 1
+            else:
+                diccionario[i] = 1
+    return diccionario
